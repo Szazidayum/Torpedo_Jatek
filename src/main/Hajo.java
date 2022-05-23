778bb5fd67ede20fb992aabffe6d346718451341
Szazidayum <szabrinamolnar@gmail.com>
2022-05-23 15:15:30 +0200

    tesztLoves() sikeres

--- src/main/Hajo.java
+++ src/main/Hajo.java
@@ -4,6 +4,17 @@ public class Hajo {
     private int[] pozicio;
     
     public String talalat(int poz){
-        return "";
+        //eldöntés
+        int i = 0;
+        while(i<3 && !(pozicio[i]==poz)){
+            i++;
+        }
+        
+        return i < 3 ? "talált" : "mellé";
     }
+
+    public Hajo(int[] pozicio) {
+        this.pozicio=pozicio;
+    }
+    
 }
--- src/main/TorpedoTeszt.java
+++ src/main/TorpedoTeszt.java
@@ -3,12 +3,14 @@ package main;
 public class TorpedoTeszt {
     public static void main(String[] args) {
         System.out.println("Teszt");
-        new TorpedoTeszt().tesztLoves(4);        //assert 1==2 : "nem jó a találat ellenőrzése";
+        new TorpedoTeszt().tesztLoves(4);        
+    //assert 1==2 : "nem jó a találat ellenőrzése";
     }
     public String tesztLoves(int poz){
-        Hajo hajo = new Hajo();
+        Hajo hajo = new Hajo(new int[] {2,3,4});
         String t = hajo.talalat(poz);
         assert t.equals("talált") : "nem jó a találat ellenőrzése";
+        
         return "";
     }
 }
