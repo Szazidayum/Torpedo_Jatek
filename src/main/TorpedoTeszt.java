424674d795857189a500ab57e063bdd38606df70
Szazidayum <szabrinamolnar@gmail.com>
2022-05-23 15:06:01 +0200

    tesztLoves() elbukott

--- src/main/TorpedoTeszt.java
+++ src/main/TorpedoTeszt.java
@@ -3,8 +3,12 @@ package main;
 public class TorpedoTeszt {
     public static void main(String[] args) {
         System.out.println("Teszt");
+        new TorpedoTeszt().tesztLoves(4);        //assert 1==2 : "nem jó a találat ellenőrzése";
     }
     public String tesztLoves(int poz){
+        Hajo hajo = new Hajo();
+        String t = hajo.talalat(poz);
+        assert t.equals("talált") : "nem jó a találat ellenőrzése";
         return "";
     }
 }
